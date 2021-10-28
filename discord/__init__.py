41f11a556d4ac2c9aef2039578ed0de3922964d8
Monke On Da Git <82736662+VincentRPS@users.noreply.github.com>
2021-10-29 07:39:53 +0800

    Update __init__.py

--- discord/__init__.py
+++ discord/__init__.py
@@ -22,7 +22,7 @@ Module to allow for backwards compatibility for existing code and extensions
 
 from nextcord import *
 
-__title__ = 'nextcord'
+__title__ = 'discord'
 __author__ = 'tag-epic & Rapptz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2021 Rapptz And 2021-present tag-epic'
