ae44f80344b4c7cfc88747c89f9bc67702257297
Monke On Da Git <82736662+VincentRPS@users.noreply.github.com>
2021-10-29 07:54:25 +0800

    Update examples/sub_commands.py
    Co-authored-by: Oliver Wilkes <34910574+ooliver1@users.noreply.github.com>

--- examples/sub_commands.py
+++ examples/sub_commands.py
@@ -4,7 +4,7 @@ from nextcord.ext import commands
 bot = commands.Bot(command_prefix="/")
 
 @bot.slash_command(guild_ids=[GUILD_ID]) # Making the command and limiting the guilds
-async def main(interaction): #passing through interaction and indentifing the sub-command group name
+async def main(interaction): # passing through interaction and indentifing the sub-command group name
     await interaction.response.send_message("This will never get called if this has subcommands.") # An Function Never Called
 
 
