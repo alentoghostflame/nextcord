cf8bbbe724f453616a9934c0172cb19af90a3fbb
Monke On Da Git <82736662+VincentRPS@users.noreply.github.com>
2021-10-28 03:24:19 -0700

    Update InteractionResponse.py

--- examples/InteractionResponse.py
+++ examples/InteractionResponse.py
@@ -1,9 +1,11 @@
 import nextcord
 from nextcord.ext import commands
 
-client = commands.Bot()
+bot = commands.Bot()
 
 #This Example Can Be Found On The Docs Aswell.
-@client.slash_command(name="ping", guild_ids=[GUILD_ID]) # Call The Function Define The Name If You Need It For Dynamic Help Commands And Which Guilds Get The Slash Command.
+@bot.slash_command(name="ping", guild_ids=[GUILD_ID]) # Call The Function Define The Name If You Need It For Dynamic Help Commands And Which Guilds Get The Slash Command.
 async def ping(interaction): # Passing Through interaction And The Name.
     await interaction.response.send_message("Pong!") # Send The Response, Please Don't Use CTX.
+    
+bot.run("TOKEN")
