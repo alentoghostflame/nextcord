abd97c2a44d29ee1d656ee3a56a89298248bf2da
Vincent <BunnyMaiV2@gmail.com>
2021-11-04 22:30:48 -0700

    Added Message Command Example

---
+++ examples/message_command.py
@@ -0,0 +1,11 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord.application_command import user_command
+
+bot = commands.Bot(command_prefix="/")
+
+@bot.message_command(name="dump")
+async def messagedump(interaction, message: Message):
+    await interaction.response.send_message(f"Data Dump: {interaction.data}")
+    
+bot.run("TOKEN")  
