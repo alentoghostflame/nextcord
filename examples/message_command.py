c46a4adab820f003d481ce1571ad50ae43379167
RPS <vincentbusiness55@gmail.com>
2021-12-01 22:26:54 -0800

    Fix

--- examples/message_command.py
+++ examples/message_command.py
@@ -5,7 +5,7 @@ from nextcord.application_command import user_command
 bot = commands.Bot(command_prefix="/")
 
 @bot.message_command(name="dump")
-async def messagedump(interaction, message: Message):
+async def messagedump(interaction, message):
     await interaction.response.send_message(f"Data Dump: {interaction.data}")
     
 bot.run("TOKEN")  
