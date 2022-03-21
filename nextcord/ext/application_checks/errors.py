c323e1df0fbc377a3d0c27b0686b46848165df92
FlameyosFlow <49708496+EyadLordXxXxX@users.noreply.github.com>
2022-03-22 00:08:16 +0200

    Fixed BucketType not ApplicationBucketType

--- nextcord/ext/application_checks/errors.py
+++ nextcord/ext/application_checks/errors.py
@@ -311,23 +311,3 @@ class ApplicationCommandOnCooldown(ApplicationError):
         self.retry_after: float = retry_after
         self.type: ApplicationBucketType = type
         super().__init__(f'You are on cooldown. Try again in {retry_after:.2f}s')
-
-class ApplicationMaxConcurrencyReached(ApplicationError):
-    """Exception raised when the command being invoked has reached its maximum concurrency.
-    This inherits from :exc:`ApplicationError`.
-    Attributes
-    ------------
-    number: :class:`int`
-        The maximum number of concurrent invokers allowed.
-    per: :class:`.ApplicationBucketType`
-        The bucket type passed to the :func:`.max_concurrency` decorator.
-    """
-
-    def __init__(self, number: int, per: ApplicationBucketType) -> None:
-        self.number: int = number
-        self.per: BucketType = per
-        name = per.name
-        suffix = 'per %s' % name if per.name != 'default' else 'globally'
-        plural = '%s times %s' if number > 1 else '%s time %s'
-        fmt = plural % (number, suffix)
-        super().__init__(f'Too many people are using this command. It can only be used {fmt} concurrently.')
